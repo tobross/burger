d468bbbd80106bb60ef0523fdcc9088dd1b7e7bc
Tobey Ross <tobross@gmail.com>
2018-05-29 22:23:47 -0500

    added jaws

--- config/connections.js
+++ config/connections.js
@@ -12,6 +12,12 @@ app.set("view engine", "handlebars");
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if(process.env.JAWSDB_URL){
+
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+}else{
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -19,7 +25,7 @@ var connection = mysql.createConnection({
   password: "",
   database: "burger_db"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
